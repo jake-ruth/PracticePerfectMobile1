af3be7325d9b5b27102a5b97debfe116bf27908d
Jake Ruth <jakeruthmusic@gmail.com>
2020-04-10 11:22:20 -0400

    working on firebase login

---
+++ PracticePerfectMobile1/AuthView.swift
@@ -0,0 +1,21 @@
+//
+//  AuthView.swift
+//  PracticePerfectMobile1
+//
+//  Created by Admin on 4/10/20.
+//  Copyright © 2020 JakeRuthMusic. All rights reserved.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
---
+++ PracticePerfectMobile1/SessionStore.swift
@@ -0,0 +1,9 @@
+//
+//  SessionStore.swift
+//  PracticePerfectMobile1
+//
+//  Created by Admin on 4/10/20.
+//  Copyright © 2020 JakeRuthMusic. All rights reserved.
+//
+
+import Foundation
